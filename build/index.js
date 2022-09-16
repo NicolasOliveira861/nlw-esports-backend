66f192bf3ffe74461d8c7353b5f7fa9098026788
Nicolas Oliveira <nicksouza861@gmail.com>
2022-09-15 23:31:46 -0300

    feat: creates backend with prisma

--- build/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/ads', (req, res) => {
-    return res.json({
-        name: 'teste',
-    });
-});
-app.listen(8080, () => console.log('Server running on http://localhost:8080'));
